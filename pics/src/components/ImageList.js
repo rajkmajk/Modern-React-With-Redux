0ce7030d3df6f6009238f57484c3ef77d31e922f
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-08 22:09:21 +0100

    css grid and react ref added

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css';
 import React from "react";
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     //we are asigning the key to the root element
     //if there was a div wraping an img tag, we'd put a key there
-   const images = props.images.map((image) => {
-        return <img alt={image.description} key={image.id} src={image.urls.regular} />
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     });
 
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
