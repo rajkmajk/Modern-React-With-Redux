c9be3a45deb010714c7ff8f108c018fffa14690e
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-18 14:38:50 +0100

    displaying videos

--- videos/src/components/videoDetail.js
+++ videos/src/components/videoDetail.js
@@ -4,8 +4,21 @@ const VideoDetail = ({video}) => {
     if (!video) {
         return <div>Loading...</div>;
     }
+
+//source link for iframe    
+const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     
-    return <div>{video.snippet.title}</div>
+    return (
+    <div>
+        <div className="ui embed">
+            <iframe src={videoSrc} />
+        </div>
+        <div className="ui segment">
+            <h4 className="ui header">{video.snippet.title}</h4>
+            <p>{video.snippet.description}</p>
+        </div>
+     </div>
+     );
 };
 
 export default VideoDetail;
