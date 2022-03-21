bf2eea3f06ff37b1b979c5ab91268fe43f28d7b6
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-21 22:25:55 +0100

    adding list

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+           const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -15,11 +16,26 @@ const Search = () => {
                     srsearch: term,
                 },
             });
+
+            setResults(data.query.search);
         };
 
         search();
 
     }, [term]); 
+
+    const renderedResults = results.map((result) => {
+        return (
+        <div key={result.pageid} className="item">
+            <div className="content">
+                <div className="header">
+                    {results.title}
+                </div>
+                {results.snippet}
+            </div>
+        </div>
+        );
+    });
 
    return (
        <div>
@@ -32,6 +48,9 @@ const Search = () => {
                     onChange={e => setTerm(e.target.value)}
                      />
                </div>
+           </div>
+           <div className="ui celled list">
+               {renderedResults}
            </div>
        </div>
    );
