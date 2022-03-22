32beead63a52b5ab81a9435a098288bf7a07f067
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-22 15:06:32 +0100

    fixing search

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,7 +20,19 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        search();
+        if ( term && !results.length) {
+            search();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500);
+    
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
 
     }, [term]); 
 
