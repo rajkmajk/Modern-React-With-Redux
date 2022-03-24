35e1ab87d2c61ec485368742bca0993772280317
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-24 21:09:12 +0100

    adding convert component

---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+const Convert = ({ language, text }) => {
+    useEffect(() => {
+
+    }, [language, text])
+    return <div />;
+};
+
+export default Convert;
