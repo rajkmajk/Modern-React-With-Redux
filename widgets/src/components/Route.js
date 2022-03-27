f1606e65f3c8bae8ea7a81c63a70265edcdf1a11
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-27 19:17:25 +0200

    changing urls and detecting navigation

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,7 +1,23 @@
 //we only need to import React when we write JSX
 // import React from "react";
+import { useEffect } from "react";
 
 const Route = ({ path, children }) => {
+    useEffect(() => {
+        
+        const onLocationChange = () => {
+            console.log('aaa');
+        };
+
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
+    }, []);
+
+
     return window.location.pathname === path
     ? children
     : null;
