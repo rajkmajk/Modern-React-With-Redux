4ba525c89ceef8cf260a1026245012fb80a8eb61
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-27 18:44:01 +0200

    adding header

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,6 +4,8 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
+
 
 const items = [
     {
@@ -35,35 +37,12 @@ const options = [
     }
 ];
 
-//route mapping without React Router
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} />;
-    }
-};
-
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />;
-    }
-};
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown />;
-    }
-};
-
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate />;
-    }
-};
 
 export default () => {
     const [selected, setSelected] = useState(options[0]);
     return (
         <div>
+            <Header />
             <Route path="/">
                 <Accordion items={items} />
             </Route>
