40f52b3168ac369749579fd4d2c6342586a58f84
rajkmajk <mihailo.rakic@protonmail.com>
2022-03-27 19:28:43 +0200

    handling command clicks

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
     const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        };
+
+
         event.preventDefault();
         window.history.pushState({}, '', href);
 
